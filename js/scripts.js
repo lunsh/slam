95f31cc6332b2a211ee9d6cb932728a19dbad323
Steve Pomeroy <steve@staticfree.info>
2016-11-17 15:29:31 -0500

    Remove Suzanne Ahrens

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,6 @@ $(document).ready(function(){
     { name: "Barbara Marder", artistUrl: "http://barbaramarder.com", photo: "barbaram.jpg" },
     { name: "Seeds of Joie", artistUrl: "https://www.etsy.com/people/SeedsOfJoie", photo: "chiey.jpg" },
     { name: "Winter Hill Jewelry", artistUrl: "https://www.etsy.com/shop/WinterHillJewelry", photo: "christ.jpg" },
-    { name: "Suzanne Ahrens", artistUrl: "http://verdecosi.com/", photo: "suzannea.jpg" },
     { name: "Saphron", artistUrl: "https://www.behance.net/faetea", photo: "allisons.jpg" },
     { name: "Miles Donovan", artistUrl: "https://www.etsy.com/shop/TheDailyRobot", photo: "milesd.jpg" },
     //{ name: "Steve Pomeroy", artistUrl: "http://staticfree.info/", photo: "stevep.jpg" }
